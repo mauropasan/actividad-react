5f9ce926a85048e18b8d471bc6d238811bd6d164
Mauro Pastor Anduix <mauropasan@ikmail.com>
2025-05-25 17:34:24 +0200

    [DEL] Remove ;

--- src/stores/index.jsx
+++ src/stores/index.jsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { create } from "zustand";
+import axios from "axios"
+import { create } from "zustand"
 
 export const useStore = create((set) => ({
   data: null,
